b99deb137ef86c0f7315909e1dce82409a2b2ef6
szilviaaltorjai <53577939+szilviaaltorjai@users.noreply.github.com>
2019-09-14 18:30:31 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,69 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.graph_objs as go
+import pandas as pd
+
+########### Define your variables ######
+
+tabtitle = 'DC Housing'
+myheading='Analysis of housing prices in Washington DC'
+neighborhood='Petworth'
+color1='#04F9E6'
+color2='#1B03B1'
+sourceurl = 'https://www.kaggle.com/christophercorrea/dc-residential-properties/'
+githublink = 'https://github.com/austinlasseter/dash-scatterplot-housing'
+
+########### Prepare the dataframe
+df = pd.read_csv('DC_Properties.csv')
+df=df[df['ASSESSMENT_NBHD']==neighborhood]
+df=df[(df['PRICE']<=1000000) & (df['PRICE']>=10000)]
+df=df[df['LANDAREA']<4000]
+df=df[df['PRICE']<900000]
+df=df[df['BEDRM']<8]
+
+########### Set up the chart
+trace = go.Scatter(
+    x = df['LIVING_GBA'],
+    y = df['PRICE'],
+    mode = 'markers',
+    marker=dict(
+        size=8,
+        color = df['BATHRM'], # set color equal to a third variable
+        colorscale=[color1, color2],
+        colorbar=dict(title='Bathrooms'),
+        showscale=True
+    )
+)
+
+data = [trace]
+layout = go.Layout(
+    title = f'Larger homes cost more in {neighborhood}!', # Graph title
+    xaxis = dict(title = 'Sales Price'), # x-axis label
+    yaxis = dict(title = 'Square Feet'), # y-axis label
+    hovermode ='closest' # handles multiple points landing on the same vertical
+)
+fig = go.Figure(data=data, layout=layout)
+
+########### Initiate the app
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
+app.title=tabtitle
+
+########### Set up the layout
+app.layout = html.Div(children=[
+    html.H1(myheading),
+    dcc.Graph(
+        id='figure-1',
+        figure=fig
+    ),
+    html.A('Code on Github', href=githublink),
+    html.Br(),
+    html.A("Data Source", href=sourceurl),
+    ]
+)
+
+############ Deploy
+if __name__ == '__main__':
+    app.run_server()
