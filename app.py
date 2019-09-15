e0e6b95fbf5e9bb0453c7ff9cfbab0ccc134cf74
szilviaaltorjai <szilviaaltorjai@gmail.com>
2019-09-14 21:42:47 -0400

    changed

--- app.py
+++ app.py
@@ -6,38 +6,14 @@ import pandas as pd
 
 ########### Define your variables ######
 
-tabtitle = 'Diabetes and poverty'
-myheading='Scatter of diabetes and poverty in US counties'
-color1='#04F9E6'
-color2='#1B03B1'
-sourceurl = 'https://www.kaggle.com/christophercorrea/dc-residential-properties/'
+tabtitle = 'the district'
+myheading1 = 'Food Atlas'
+myheading2 = 'Diabetes in US counties'
+image1 = 'Hist_diabetes.jpg'
+image2 = 'Pov_diabetes.jpg'
+image3 = 'Diabetes_Pov_Foodtax.jpg'
+sourceurl = 'https://www.ers.usda.gov/data-products/food-environment-atlas/data-access-and-documentation-downloads/'
 githublink = 'https://github.com/szilviaaltorjai/Food_Atlas'
-
-########### Prepare the dataframe
-df = pd.read_csv('Food_clean.csv')
-
-########### Set up the chart
-trace = go.Scatter(
-    x = df['POVRATE15'],
-    y = df['PCT_DIABETES_ADULTS13'],
-    mode = 'markers',
-    marker=dict(
-        size=8,
-        color = df['FOOD_TAX14'], # set color equal to a third variable
-        colorscale=[color1, color2],
-        colorbar=dict(title='Food tax'),
-        showscale=True
-    )
-)
-
-data = [trace]
-layout = go.Layout(
-    title = f'Scatterplot of poverty rates and proportion of adults with diabetes, US counties', # Graph title
-    xaxis = dict(title = 'Poverty rates'), # x-axis label
-    yaxis = dict(title = 'Proportion of adults with diabetes'), # y-axis label
-    hovermode ='closest' # handles multiple points landing on the same vertical
-)
-fig = go.Figure(data=data, layout=layout)
 
 ########### Initiate the app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -47,11 +23,20 @@ app.title=tabtitle
 
 ########### Set up the layout
 app.layout = html.Div(children=[
-    html.H1(myheading),
-    dcc.Graph(
-        id='figure-1',
-        figure=fig
-    ),
+    html.H1(myheading1),
+    html.H2(myheading2),
+    html.Div([
+        html.Div([
+            html.Img(src=app.get_asset_url(image1), style={'width': '50%', 'height': 'auto'})
+        ],className='three columns'),
+        html.Div([
+            html.Img(src=app.get_asset_url(image2), style={'width': '80%', 'height': 'auto'}),
+        ],className='three columns'),
+        html.Div([
+            html.Img(src=app.get_asset_url(image3), style={'width': '50%', 'height': 'auto'})
+        ],className='three columns'),
+        ],className='six columns'),
+    ],className='twelve columns'),
     html.A('Code on Github', href=githublink),
     html.Br(),
     html.A("Data Source", href=sourceurl),
