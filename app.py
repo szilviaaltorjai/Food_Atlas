39767a077df6997bd3c60adfedc69fc9919efef7
szilviaaltorjai <szilviaaltorjai@gmail.com>
2019-09-19 13:02:06 -0400

    test

--- app.py
+++ app.py
@@ -1,12 +1,45 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import os
+import plotly.graph_objs as go
+import pandas as pd
 
 ########### Define your variables ######
 
-list_of_choices=['Distribution of adults with diabetes', 'Daibetes and poverty in US counties', 'Diabetes, poverty and food tax']
+tabtitle = 'Diabetes in US counties, 2013'
+myheading = 'Scatterplot of diabetes prevalence among adults and pverty rate in US counties'
+color1='#04F9E6'
+color2='#1B03B1'
+sourceurl = 'https://www.kaggle.com/christophercorrea/dc-residential-properties/'
 githublink = 'https://github.com/szilviaaltorjai/Food_Atlas'
+
+#### Prepare dataframe
+
+df = pd.read_csv('Food_clean.csv')
+
+#### Set up the chart
+trace = go.Scatter(
+    x=df['POVRATE15'],
+    y=df['PCT_DIABETES_ADULTS13'],
+    mode = 'markers',
+    marker=dict(
+        size=8,
+        color=df['FOOD_TAX14'],
+        colorscale=[color1, color2],
+        colorbar=dict(title='Food tax'),
+        showcase=True
+    )
+)
+
+data=[trace]
+layout = go.Layout(
+    title=f'Poor areas have higher diabetes prevalence and higher food taxes.,
+    xaxis=dict(title = 'Poverty rate'),
+    yaxis=dict(title = 'Proportion of adults with diabetes'),
+    hovermode = 'closest'
+)
+
+fig=go.Figure(data=data, layout=layout)
 
 ########### Initiate the app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -15,26 +48,18 @@ server = app.server
 app.title='FoodAtlas'
 
 ########### Set up the layout
-app.layout = html.Div([
-    html.H2('Food Atlas in the US'),
-    html.Img(src=app.get_asset_url(image1), style={'width': 'auto', 'heights': '10%'}),
-    dcc.Dropdown(id='your-input-here',
-                options=[{'label': i, 'value': i} for i in list_of_choices],
-                value='Diabetes, poverty and food tax',
-                style={'width': '500px'}),
+app.layout = html.Div(children=[
+    html.H1(myheading),
+    dcc.Graph(
+        id='figure-1',
+        figure=fig
+    ),
+    html.A('Code on Github', href=githublink),
     html.Br(),
-    html.Div(id='your-output-here', children=''),
-    html.Br(),
-    html.A('Code on Github'', href=githublink),
-
-])
-
-############### Interactive callback comes here
-@app.callback(dash.dependencies.Output('your-output-here', 'children'),
-                [dash.dependencies.Input('your-input-here', 'value')])
-def display_value(whatever_you_chose):
-    return f'Food Atlas shows {whatever_you_chose}.'
+    html.A("Data Source", href=sourceurl),
+    ]
+)
 
 ############ Deploy
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server()
