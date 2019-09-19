f5af06693ff4fd1d44041961558c413d2471b7cd
szilviaaltorjai <szilviaaltorjai@gmail.com>
2019-09-19 10:14:59 -0400

    test

--- app.py
+++ app.py
@@ -1,47 +1,40 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import plotly.graph_objs as go
-import pandas as pd
+import os
 
 ########### Define your variables ######
 
-tabtitle = 'the district'
-myheading1 = 'Food Atlas'
-myheading2 = 'Diabetes in US counties'
-image1 = 'Hist_diabetes.jpg'
-image2 = 'Pov_diabetes.jpg'
-image3 = 'Diabetes_Pov_Foodtax.jpg'
-sourceurl = 'https://www.ers.usda.gov'
+list_of_choices=['Distribution of adults with diabetes', 'Daibetes and poverty in US counties', 'Diabetes, poverty and food tax']
 githublink = 'https://github.com/szilviaaltorjai/Food_Atlas'
 
 ########### Initiate the app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
-app.title=tabtitle
+app.title='FoodAtlas'
 
 ########### Set up the layout
-app.layout = html.Div(children=[
-    html.H1(myheading1),
-    html.H2(myheading2),
-    html.Div([
-        html.Div([
-            html.Img(src=app.get_asset_url(image1), style={'width': '75%', 'height': 'auto'})
-        ],className='four columns'),
-        html.Div([
-            html.Img(src=app.get_asset_url(image2), style={'width': '75%', 'height': 'auto'}),
-        ],className='four columns'),
-        html.Div([
-            html.Img(src=app.get_asset_url(image3), style={'width': '75%', 'height': 'auto'})
-        ],className='four columns'),
-    ],className='twelve columns'),
-    html.A('Code on Github', href=githublink),
+app.layout = html.Div([
+    html.H2('Food Atlas in the US'),
+    html.Img(src=app.get_asset_url(image1), style={'width': 'auto', 'heights': '10%'}),
+    dcc.Dropdown(id='your-input-here',
+                options=[{'label': i, 'value': i} for i in list_of_choices],
+                value='Diabetes, poverty and food tax',
+                style={'width': '500px'}),
     html.Br(),
-    html.A("Data Source", href=sourceurl),
-    ]
-)
+    html.Div(id='your-output-here', children=''),
+    html.Br(),
+    html.A('Code on Github'', href=githublink),
+
+])
+
+############### Interactive callback comes here
+@app.callback(dash.dependencies.Output('your-output-here', 'children'),
+                [dash.dependencies.Input('your-input-here', 'value')])
+def display_value(whatever_you_chose):
+    return f'Food Atlas shows {whatever_you_chose}.'
 
 ############ Deploy
 if __name__ == '__main__':
-    app.run_server()
+    app.run_server(debug=True)
