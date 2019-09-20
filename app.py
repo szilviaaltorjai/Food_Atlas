f3908a499eab5cc784ef3f6167d7d68b1459c083
szilviaaltorjai <szilviaaltorjai@gmail.com>
2019-09-20 12:34:38 -0400

    test

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import pandas as pd
 tabtitle = 'Diabetes in US counties, 2013'
 myheading = 'Scatterplot of diabetes prevalence among adults and poverty rate in US counties'
 color1='#04F9E6'
-sourceurl = 'https://www.ers.usda.gov/data-products/food-environment-atlas/data-access-and-documentation-downloads/'
+sourceurl = 'https://www.ers.usda.gov'
 githublink = 'https://github.com/szilviaaltorjai/Food_Atlas'
 
 #### Prepare dataframe
@@ -24,7 +24,6 @@ trace = go.Scatter(
     y=df['PCT_DIABETES_ADULTS13'],
     mode = 'markers',
     )
-)
 
 data=[trace]
 layout = go.Layout(
