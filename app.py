9c68ca619c5027b8e5f3f109c7cfb075f86def64
szilviaaltorjai <szilviaaltorjai@gmail.com>
2019-09-20 13:41:25 -0400

    changed

--- app.py
+++ app.py
@@ -23,6 +23,11 @@ trace = go.Scatter(
     x=df['POVRATE15'],
     y=df['PCT_DIABETES_ADULTS13'],
     mode = 'markers',
+    marker=dict(
+        size=8,
+        colorscale=[color1],
+        showscale=True
+    )
 )
 
 data=[trace]
@@ -43,25 +48,16 @@ app.title=tabtitle
 
 ########### Set up the layout
 app.layout = html.Div(children=[
-    html.H1(myheading1),
-    html.H2(myheading2),
-    html.Div([
-        html.Div([
-            html.Img(src=app.get_asset_url(image1), style={'width': '75%', 'height': 'auto'})
-        ],className='four columns'),
-        html.Div([
-            html.Img(src=app.get_asset_url(image2), style={'width': '75%', 'height': 'auto'}),
-        ],className='four columns'),
-        html.Div([
-            html.Img(src=app.get_asset_url(image3), style={'width': '75%', 'height': 'auto'})
-        ],className='four columns'),
-    ],className='twelve columns'),
+    html.H1(myheading),
+    dcc.Graph(
+        id='figure-1',
+        figure=fig
+    ),
     html.A('Code on Github', href=githublink),
     html.Br(),
     html.A("Data Source", href=sourceurl),
     ]
 )
-
 ############ Deploy
 if __name__ == '__main__':
-    app.run
+    app.run_server()
