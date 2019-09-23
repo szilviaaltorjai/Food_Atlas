c2fac0a07df47b7752d7b4543f9a0491fccee6a7
szilviaaltorjai <szilviaaltorjai@gmail.com>
2019-09-23 18:24:00 -0400

    fixed

--- app.py
+++ app.py
@@ -4,16 +4,45 @@ import dash_html_components as html
 import plotly.graph_objs as go
 import pandas as pd
 
+
 ########### Define your variables ######
 
-tabtitle = 'the district'
-myheading1 = 'Food Atlas'
-myheading2 = 'Diabetes in US counties'
-image1 = 'Hist_diabetes.jpg'
-image2 = 'Pov_diabetes.jpg'
-image3 = 'Diabetes_Pov_Foodtax.jpg'
+tabtitle = 'Food Atlas'
+myheading='Diabetes and poverty'
+neighborhood='counties'
+color1='#04F9E6'
+color2='#1B03B1'
 sourceurl = 'https://www.ers.usda.gov'
-githublink = 'https://github.com/szilviaaltorjai/Food_Atlas'
+githublink = 'https://github.com/szilviaaltorjair/Foos_Atlas_test'
+
+
+########### Prepare the dataframe
+df = pd.read_csv('Food_clean.csv', encoding="ISO-8859-1")
+
+########### Set up the chart
+trace = go.Scatter(
+	    x = df['POVRATE15'],
+	    y = df['PCT_DIABETES_ADULTS13'],
+	    mode = 'markers',
+	    marker=dict(
+	        size=8,
+	        color = df['FOOD_TAX14'], # set color equal to a third variable
+	        colorscale=[color1, color2],
+	        colorbar=dict(title='Food tax'),
+	        showscale=True
+	    )
+)
+
+
+data = [trace]
+layout = go.Layout(
+	    title = f'Diabetes and poverty!', # Graph title
+	    xaxis = dict(title = 'Diabetes'), # x-axis label
+	    yaxis = dict(title = 'Poverty'), # y-axis label
+	    hovermode ='closest' # handles multiple points landing on the same vertical
+	)
+fig = go.Figure(data=data, layout=layout)
+
 
 ########### Initiate the app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -21,26 +50,20 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 app.title=tabtitle
 
+
 ########### Set up the layout
 app.layout = html.Div(children=[
-    html.H1(myheading1),
-    html.H2(myheading2),
-    html.Div([
-        html.Div([
-            html.Img(src=app.get_asset_url(image1), style={'width': '75%', 'height': 'auto'})
-        ],className='four columns'),
-        html.Div([
-            html.Img(src=app.get_asset_url(image2), style={'width': '75%', 'height': 'auto'}),
-        ],className='four columns'),
-        html.Div([
-            html.Img(src=app.get_asset_url(image3), style={'width': '75%', 'height': 'auto'})
-        ],className='four columns'),
-    ],className='twelve columns'),
-    html.A('Code on Github', href=githublink),
-    html.Br(),
-    html.A("Data Source", href=sourceurl),
-    ]
+  html.H1(myheading),
+  dcc.Graph(
+	        id='figure-1',
+	        figure=fig
+	    ),
+html.A('Code on Github', href=githublink),
+html.Br(),
+html.A("Data Source", href=sourceurl),
+	    ]
 )
+
 
 ############ Deploy
 if __name__ == '__main__':
